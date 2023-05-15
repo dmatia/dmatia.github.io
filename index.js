45f19785e0e748036de8725f55876587ac664d96
dmatia <diegovillalba.96@gmail.com>
2023-05-14 23:48:32 -0300

    Agrego apartado experiencia

--- index.js
+++ index.js
@@ -1,63 +1,55 @@
+var secciones = document.querySelectorAll(".title");
+var cuerposec = document.querySelectorAll(".section_body");
+var contenedor = document.getElementById("main");
 
-var secciones = document.querySelectorAll('.title');
-var cuerposec = document.querySelectorAll('.section_body');
-var contenedor = document.getElementById('main');
+secciones.forEach((elemento, clave) => {
+  elemento.addEventListener("click", () => {
+    let clase = "";
+    switch (clave) {
+      case 0:
+        clase = "main_aboutme";
+        break;
+      case 1:
+        clase = "main_do";
+        break;
+      case 2:
+        clase = "main_experience";
+        break;
+    }
+    contenedor.classList.toggle(`${clase}`);
 
+    clase = "";
 
-secciones.forEach((elemento, clave)=>{
-    
-    elemento.addEventListener('click', () => {
-    
+    switch (clave) {
+      case 0:
+        clase = "titulo_exp";
+        break;
+      case 1:
+        clase = "do_exp";
 
-        let clase = "";
-        switch(clave) {
-            case 0:  
-                    clase = 'main_aboutme';
-            break;
-            case 1:  
-                    clase = 'main_do';
-            break;
-            case 2:  
-                    clase = 'main_experience';
-            break;
-          
-          }
-        contenedor.classList.toggle(`${clase}`);
-        
-        clase = "";
+        break;
+      case 2:
+        clase = "experience_exp";
 
-        switch(clave){
-            case 0:  
-            clase = 'titulo_exp';
-    break;
-    case 1:  
-    clase = 'do_exp';
-            
-    break;
-    case 2:  
+        break;
+    }
+    secciones[clave].classList.toggle(`${clase}`);
 
-    break;
-        }
-        secciones[clave].classList.toggle(`${clase}`)
+    cuerposec[clave].classList.toggle("visible");
 
-        cuerposec[clave].classList.toggle('visible');
-        
-        switch(clave){
-            case 0:  
-            secciones[1].classList.toggle('ocultar');
-            secciones[2].classList.toggle('ocultar');
-    break;
-    case 1:  
-    secciones[0].classList.toggle('ocultar');
-    secciones[2].classList.toggle('ocultar');
-            
-    break;
-    case 2:
-        secciones[1].classList.toggle('ocultar');
-    secciones[0].classList.toggle('ocultar');
-        }
-        
+    switch (clave) {
+      case 0:
+        secciones[1].classList.toggle("ocultar");
+        secciones[2].classList.toggle("ocultar");
+        break;
+      case 1:
+        secciones[0].classList.toggle("ocultar");
+        secciones[2].classList.toggle("ocultar");
 
-    })
-})
-
+        break;
+      case 2:
+        secciones[0].classList.toggle("ocultar");
+        secciones[1].classList.toggle("ocultar");
+    }
+  });
+});
